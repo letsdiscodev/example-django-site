593c7683c890ab6e3095ccad75c65438b3e0574f
gregsadetsky <lepetitg@gmail.com>
2024-03-17 13:57:29 -0400

    .

--- core/views.py
+++ core/views.py
@@ -5,7 +5,7 @@ from core.models import VisitCounter
 
 def index(request):
     # insert a VisitCounter
-    counter = VisitCounter.objects.create()
+    VisitCounter.objects.create()
     # get the total number of visitors
     total_visitors = VisitCounter.objects.count()
     return render(request, "core/index.html", {"total_visitors": total_visitors})
