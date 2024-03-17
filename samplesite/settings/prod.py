0aabfb2633911b7d5d30252e03874236dc8fc7a6
gregsadetsky <lepetitg@gmail.com>
2024-03-17 13:39:55 -0400

    .

--- samplesite/settings/prod.py
+++ samplesite/settings/prod.py
@@ -2,6 +2,8 @@ from .base import *
 
 DEBUG = True
 
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+
 # default logging doesn't log to console with DEBUG=False
 # see https://github.com/django/django/blob/main/django/utils/log.py
 # override i.e. always log to console
