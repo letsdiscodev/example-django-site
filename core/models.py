4ad7b9d5d052850ad2d56e67ce42cc690a9aa17b
gregsadetsky <lepetitg@gmail.com>
2024-03-17 13:51:50 -0400

    .

--- core/models.py
+++ core/models.py
@@ -5,3 +5,7 @@ from django.db import models
 # https://docs.djangoproject.com/en/4.2/topics/auth/customizing/#using-a-custom-user-model-when-starting-a-project
 class User(AbstractUser):
     pass
+
+
+class VisitCounter(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
--- core/views.py
+++ core/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
+from core.models import VisitCounter
+
 
 def index(request):
-    return render(request, "core/index.html")
+    # insert a VisitCounter
+    counter = VisitCounter.objects.create()
+    # get the total number of visitors
+    total_visitors = VisitCounter.objects.count()
+    return render(request, "core/index.html", {"total_visitors": total_visitors})
