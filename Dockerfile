c36717320ee5d63d37abf7f31504317ee74cc1b1
gregsadetsky <lepetitg@gmail.com>
2024-06-02 16:00:01 -0400

    .

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,10 @@
 FROM python:3.12.1
-ADD . /code
+
+# docker will not re-pip install if requirements.txt doesn't change
 WORKDIR /code
+ADD ./requirements.txt /code/requirements.txt
 RUN pip install -r requirements.txt
+
+ADD . /code
+
 CMD ["bin/serve.sh"]
--- examplesite/settings.py
+++ examplesite/settings.py
@@ -10,8 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 import os
 from pathlib import Path
+
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-r+lym&5!9wh0z@*uhlc6-4f$^jbfv@#df%eq-(k+8mku*5a&ov"
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -75,10 +81,10 @@ WSGI_APPLICATION = "examplesite.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 
